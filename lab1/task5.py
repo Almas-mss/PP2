be1d3badd2f4eaab6e383b5fafba83b2c77b195e
Almas Musa <almasmss@gmail.com>
2025-01-18 17:21:58 +0500

    Python Names

---
+++ lab1/task5.py
@@ -0,0 +1,17 @@
+mybook = "Math1"
+my_book = "Math2"
+_my_book = "Math3"
+myBook = "Math4"
+MYBOOK = "Math5"
+mybook7 = "math6"
+#you cant do that
+# 2mybook = "Math"
+# my-book = "Math"
+# my book = "Math"
+print(mybook)
+print(my_book)
+print(_my_book)
+print(myBook)
+print(MYBOOK)
+print(mybook7)
+
