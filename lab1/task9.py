5dbbff4bc055f08a7e7cfdec9b258c79f13e2e37
Almas Musa <almasmss@gmail.com>
2025-01-18 18:03:10 +0500

    Python data types

---
+++ lab1/task9.py
@@ -0,0 +1,51 @@
+# Text type:
+a = "Travis Scott"
+print(a)
+print(type(a))
+
+
+# Numeric Types:
+b = 13
+c = 4.6
+d = 3j
+print(b)
+print(c)
+print(d)
+print(type(b))
+print(type(c))
+print(type(d))
+
+
+# Sequence Types:
+e = ["Music", "is", "art"]
+f = ("I", "get", "it")
+g = range(7)
+print(e)
+print(f)
+print(g)
+print(type(e))
+print(type(f))
+print(type(g))
+
+
+
+# Mapping Type:
+h = {"Almas" : "KBTU", "course" : 1}
+print(h)
+print(type(h))
+
+
+
+# Set Types:
+i = {"pls drop!!","Ocean","Frank"}
+j = frozenset({"Ocean","Frank","pls drop!!"})
+print(i)
+print(j)
+print(type(i))
+print(type(j))
+
+
+
+
+
+
