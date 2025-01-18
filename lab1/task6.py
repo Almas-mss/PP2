142fcde31f3eebd1b8c373f6d61c0ca5b4f452cb
Almas Musa <almasmss@gmail.com>
2025-01-18 17:27:31 +0500

    Python Multiple Values

---
+++ lab1/task6.py
@@ -0,0 +1,15 @@
+x, y, z = "Almas", "is", "student"
+print(x)
+print(y)
+print(z)
+
+a = b = c = "Breaking bad"
+print(a)
+print(b)
+print(c)
+
+books = ["Physics", "Calculus2", "Discreate"]
+i , j , h = books
+print(i)
+print(j)
+print(h)
