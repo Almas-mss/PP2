e218f112c6158d50b7b6a6cfdb423dbdcb1a0195
Almas Musa <almasmss@gmail.com>
2025-01-18 17:15:24 +0500

    Python Variables

---
+++ lab1/task4.py
@@ -0,0 +1,23 @@
+x = 5
+y = "John"
+print(x) # x is int
+print(y) #y is str
+
+a = 12
+a = "Almass"
+print(a)
+
+b = str(5)  # will be '5'
+c = float(5) # will be 5.0
+d = int(3) #will be 5
+print(b,c,d)
+
+i = 7
+j = "labka"
+print(type(i))
+print(type(j))
+
+w = 5
+W = "KBTU"
+print(w)
+print(W)
