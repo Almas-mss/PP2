155348ad164e737d09c4f2b000aeb819b0aec9f1
Almas Musa <almasmss@gmail.com>
2025-01-18 17:42:32 +0500

    Python Global Variables

---
+++ lab1/task8.py
@@ -0,0 +1,26 @@
+x = "Macbook"
+
+def myfunc():
+    print(x+" Air")
+myfunc()
+
+
+
+x1 = "Not cool"
+
+def maybeFunc():
+  x1 = "cool"
+  print("Programming is " + x1)
+maybeFunc()
+
+print("Labka is " + x1)
+
+
+
+def notFunc():
+  global x5
+  x5 = ", The creator"
+
+notFunc()
+
+print("Tyler" + x5)
