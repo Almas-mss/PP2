42ad8c20cff23f9315c9b70361814232bcb967a9
Almas Musa <almasmss@gmail.com>
2025-01-18 18:11:53 +0500

    Python Numbers

---
+++ lab1/task10.py
@@ -0,0 +1,33 @@
+# Int
+x = 1
+y = 2783664278
+z = -876354
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# Float
+x2 = 7.843800
+y2 = 12.0
+z2 = -321.3874
+
+x3 = 98e4
+y3 = 15E9
+z3 = -76.3e200
+print(type(x2))
+print(type(y2))
+print(type(z2))
+print(type(x3))
+print(type(y3))
+print(type(z3))
+
+
+
+# Complex
+x4 = 7+5j
+y4 = 7j
+z4 = -6j
+print(type(x4))
+print(type(y4))
+print(type(z4))
