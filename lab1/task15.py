63629103df4cd77a431d62687568b4ac50a11bb8
Almas Musa <almasmss@gmail.com>
2025-01-18 19:37:03 +0500

    Python Format Strings

---
+++ lab1/task15.py
@@ -0,0 +1,13 @@
+time = "19:30"
+txt = f" Now time is {time}"
+print(txt)
+print()
+print("How mush does bread cost?")
+price = 570
+cmp = f"bread costs {price} tenge"
+print(cmp)
+print()
+print()
+
+dmd = f"the price is {20*30} dollars"
+print(dmd)
