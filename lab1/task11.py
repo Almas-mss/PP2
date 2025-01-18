c54adf41f4e11f5f391ee93f83a9c99e3334e175
Almas Musa <almasmss@gmail.com>
2025-01-18 18:23:46 +0500

    Python Casting

---
+++ lab1/task11.py
@@ -0,0 +1,32 @@
+# cast for Int
+x = int(65)
+y = int(8.98)
+z = int("2")
+
+
+# cast for Float
+x2 = float(4)
+y2 = float(4.637)
+z2 = float("5")
+w2 = float("5.35")
+
+
+# cast for String
+x3 = str("a1")
+y3 = str("34")
+z3 = str("21.98")
+
+
+
+print(x)
+print(y)
+print(z)
+print(x2)
+print(y2)
+print(z2)
+print(w2)
+print(x3)
+print(y3)
+print(z3)
+
+
