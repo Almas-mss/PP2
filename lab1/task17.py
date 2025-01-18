5d3ad8769052b4c20db315a167f565563a143302
Almas Musa <almasmss@gmail.com>
2025-01-18 20:05:50 +0500

    Python String Methods

---
+++ lab1/task17.py
@@ -0,0 +1,21 @@
+
+a = "converts"
+print(a.capitalize())
+
+b = "Hello My Classs!"
+print(b.casefold())
+
+c = "Almaty"
+print(c.center(17))
+
+d = "Apple Apple Appllle Aple Apple apple ApplE Apple Apple banana fruit"
+print(d.count("Apple"))
+
+e = "Good bie professor!"
+print(e.endswith("!"))
+
+f = "A\tL\tM\tA\tS\t"
+print(f.expandtabs(3))
+
+e = "21 Savage is really savage"
+print(e.find("really"))
