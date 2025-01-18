5d93cedd82dd181c29da9f7e87c59197ca905de0
Almas Musa <almasmss@gmail.com>
2025-01-18 19:51:37 +0500

    Python Escape Characters

---
+++ lab1/task16.py
@@ -0,0 +1,14 @@
+txt = "My name\'s Almas"
+print(txt)
+pnp = "it is backslash \\"
+print(pnp)
+cmp = "it is new \n line!"
+print(cmp)
+jtk = "Hello\tFriend!"
+print(jtk)
+pup = "hello my  \bFriend"
+print(pup)
+pop = "\110\145\154\154\157"
+print(pop)
+prp = "\x48\x65\x6c\x6c\x6f"
+print(prp)
