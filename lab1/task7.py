fc39d8d43885a4924bab2105cb637289f2bebf74
Almas Musa <almasmss@gmail.com>
2025-01-18 17:34:31 +0500

    Python Just Variables

---
+++ lab1/task7.py
@@ -0,0 +1,20 @@
+x = "KBTU is the best"
+print(x)
+
+x1 = "K"
+x2 = "B"
+x3 = "T"
+x4 = "U"
+print(x1,x2,x3,x4)
+#or
+print(x1+x2+x3+x4)
+
+x5 = 19
+x6 = 26
+print(x5+x6)
+
+x7 = 3
+x8 = "Almas"
+#you cant do that
+#print(x7+x8)
+print(x7,x8)
