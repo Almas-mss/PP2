8ec7a5cecec0ac14e3835994226e3cfe676dee24
Almas Musa <almasmss@gmail.com>
2025-01-23 13:38:02 +0500

    Python While Loops

---
+++ Lab2/Task_8.py
@@ -0,0 +1,38 @@
+# While Loops
+
+a = 0
+while a <= 10:
+    print(a)
+    a+=1
+
+print()
+print()
+
+i = 100
+while i < 999:
+    print(i)
+    if i == 200:
+        break
+    i+=10
+
+print()
+print()
+
+j = 0
+while j <= 15 :
+    j+=1
+    if j == 10:
+        continue
+    print(j)
+
+print()
+print()
+
+b = 1
+while b < 6:
+  print(b)
+  b += 1
+else:
+  print("i is no longer less than 6")
+
+
