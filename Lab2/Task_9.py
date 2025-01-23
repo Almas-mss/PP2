351adcf42ac7295d249d0dc4339ca28104d9ad5f
Almas Musa <almasmss@gmail.com>
2025-01-23 13:46:01 +0500

    Python For loops

---
+++ Lab2/Task_9.py
@@ -0,0 +1,29 @@
+# For Loops
+
+for x in "ALMAS":
+  print(x)
+
+print()
+print()
+
+
+for x in range(3, 40, 4):
+  print(x)
+
+print()
+print()
+
+for x in range(6):
+  print(x)
+else:
+  print("Finally finished!")
+
+print()
+print()
+
+adj = ["Almas", "Daulet", "Dosken"]
+fruits = ["KBTU", "AUES", "ALMAU"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
